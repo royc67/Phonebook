20edfb605d4f14090d68974f4732b0609ad2642a
royc67 <royc67@gmail.com>
2020-09-03 16:20:42 +0300

    Morgan
    Added morgan dev logger.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 app.use(express.json());
+app.use(morgan('dev'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
