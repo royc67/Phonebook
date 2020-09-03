cdc9d79308eec83fa690f0efae929d3ae99976b9
royc67 <royc67@gmail.com>
2020-09-03 15:05:11 +0300

    requestLogger
    Added a logger that runs as a middleware.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
 let phoneBook = [
     {name: "abc", number: "052-0000000", id: 1},
     {name: "def", number: "052-1111111", id: 2},
