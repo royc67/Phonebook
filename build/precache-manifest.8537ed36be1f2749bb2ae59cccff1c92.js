29fe32d379c84402914b1a394686c04f80754df6
royc67 <royc67@gmail.com>
2020-09-04 14:42:24 +0300

    uibuild

---
+++ build/precache-manifest.8537ed36be1f2749bb2ae59cccff1c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c1e7b59fda8a0e8f33474bf5a5f87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "332f4b6ec841367b0dad",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b760dc6156344fdb1767",
+    "url": "/static/js/2.7763b8f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7763b8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332f4b6ec841367b0dad",
+    "url": "/static/js/main.75b005d1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
